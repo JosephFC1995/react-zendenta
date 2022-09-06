9969ddb8c19362693423344a8fa94e74e341972a
Joseph <j.flores@comunal.co>
2022-09-06 09:14:13 -0500

    Feat: update html with styled component.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/assets/scss/main.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Calendar from "./views/Calendar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/app" element={<App />}>
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="calendar" element={<Calendar />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
